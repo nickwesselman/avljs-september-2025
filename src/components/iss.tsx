30ecaf9062e238cbaac54000b87d15520ccbbbcc
Nick Wesselman <me@nickwesselman.com>
2025-09-09 23:59:57 -0400

    added iss location

--- src/components/iss.tsx
+++ src/components/iss.tsx
@@ -3,6 +3,7 @@ import {Text} from 'ink'
 
 const IssPosition = () => {
     const [position, setPosition] = useState([0,0])
+    const [location, setLocation] = useState('unknown')
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,8 +20,20 @@ const IssPosition = () => {
         fetchLoop(500)
     }, [])
 
+    useEffect(() => {
+        if (!position || !position[0] || position[0] == 0) {
+            return
+        }
+        const fetchLocation = async() => {
+            const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=2b872672811d4f93b957f3f0f58a392a&q=${position[0]},${position[1]}&pretty=1&no_annotations=1`)
+            const data = await response.json()
+            setLocation(data.results[0]?.formatted)
+        }
+        fetchLocation()
+    }, [position])
+
     return (
-        <Text color="green">{position[0]},{position[1]}</Text>
+        <Text color="green">{position[0]},{position[1]} ({location})</Text>
     )
 }
 
